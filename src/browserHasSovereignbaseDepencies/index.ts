c6c7c71e029a2ccaeaf51bd454d21b5a6fb22884
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-05 17:39:48 +0300

    chore: development

--- src/browserHasSovereignbaseDepencies/index.ts
+++ src/browserHasSovereignbaseDepencies/index.ts
@@ -4,12 +4,16 @@ export async function browserHasSovereignbaseDepencies(): Promise<boolean> {
     window.isSecureContext &&
     typeof navigator !== 'undefined' &&
     typeof navigator.credentials !== 'undefined' &&
-    'showDirectoryPicker' in window &&
     'indexedDB' in window &&
     'BroadcastChannel' in window &&
     'WebSocket' in window &&
+    'AbortSignal' in window &&
     'EventTarget' in window &&
     'CustomEvent' in window &&
+    'MessageEvent' in window &&
+    'DOMException' in window &&
+    'locks' in navigator &&
+    'onLine' in navigator &&
     'Worker' in window &&
     'serviceWorker' in navigator &&
     'PushManager' in window &&
