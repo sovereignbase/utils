25870a91aaae414749ae8c284129d5c52e4b15bf
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-05 04:02:29 +0300

    chore: development

--- src/browserHasSovereignbaseDepencies/index.ts
+++ src/browserHasSovereignbaseDepencies/index.ts
@@ -10,8 +10,13 @@ export async function browserHasSovereignbaseDepencies(): Promise<boolean> {
     'WebSocket' in window &&
     'EventTarget' in window &&
     'CustomEvent' in window &&
+    'Worker' in window &&
+    'serviceWorker' in navigator &&
+    'PushManager' in window &&
+    'Notification' in window &&
     typeof crypto !== 'undefined' &&
     !!crypto.subtle &&
+    typeof crypto.randomUUID === 'function' &&
     typeof crypto.getRandomValues === 'function' &&
     'PublicKeyCredential' in window &&
     typeof PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable ===
